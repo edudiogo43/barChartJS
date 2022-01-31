fcd2bcf4e9e32bb4aad9fcad169070e533843e6a
Eduardo D. Garcia <89088033+edudiogo43@users.noreply.github.com>
2022-01-31 00:38:32 -0300

    added legend with percentage

--- script.js
+++ script.js
@@ -13,13 +13,24 @@ submit.addEventListener("click", () => {
     const chartValues = textValues.split(",");
     
     let width = 40;
-    let x = 20;
+    let x = 40;
+
+    const reducer = (accumulator, curr) => parseFloat(accumulator) + parseFloat(curr);
+    let total = chartValues.reduce(reducer);
     
     chartValues.forEach((value) => {
         let h = value;
-        ctx.fillRect(x, canvas.height / 2 - h , width, h);
+        const posY = canvas.height / 1.6 - h;
+        ctx.fillRect(x, posY, width, h);
         ctx.fillStyle = `hsla(${Math.random() * 360}, 50%, 50%, 1)`;
         x += width+20;
+
+        // Adding legend
+        ctx.font = "14px Arial";
+
+        let percentage = (h/total) * 100;
+        ctx.fillText(percentage.toFixed(2)+'%', x-60, 590);
+
     })
 
 })
