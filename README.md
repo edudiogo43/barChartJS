1d3a0e2825d35eeebfed2697876aace3a24a6230
Eduardo D. Garcia <89088033+edudiogo43@users.noreply.github.com>
2022-01-31 00:05:55 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# BarChart-JS
+An example and very simplistic of how to create a bar chart using only Vanilla javascript.
+
+## How to use
+Just open the index.html file and update the values of text input field by separating them by semicolon character.
+
+## Example
+<img src="img.png">
+
+
+## License
+MIT License
+
+## Author
+Eduardo Diogo Garcia /
+eduardo.diogo@gmail.com
---
+++ script.js
@@ -0,0 +1,25 @@
+const canvas = document.querySelector("canvas");
+const submit = document.querySelector("#btnSubmit");
+const ctx = canvas.getContext("2d");
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+submit.addEventListener("click", () => {
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    const textValues = document.querySelector("#txtValues").value;
+    const chartValues = textValues.split(",");
+    
+    let width = 40;
+    let x = 20;
+    
+    chartValues.forEach((value) => {
+        let h = value;
+        ctx.fillRect(x, canvas.height / 2 - h , width, h);
+        ctx.fillStyle = `hsla(${Math.random() * 360}, 50%, 50%)`;
+        x += width+20;
+    })
+
+})
